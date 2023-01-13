7bc91d38d3e1d9a6728c7fef4284618c18aa6ce8
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-01-13 21:21:22 +0530

    Contoller unit testing competed using mockito

--- src/main/java/com/springboot/testing/springboottesting/controller/EmployeeController.java
+++ src/main/java/com/springboot/testing/springboottesting/controller/EmployeeController.java
@@ -1,7 +1,15 @@
 package com.springboot.testing.springboottesting.controller;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.mustache.MustacheProperties.Reactive;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,11 +26,45 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee creatEmployee( @RequestBody Employee employee){
-         return employeeService.saveEmployee(employee);
+    public Employee creatEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
     }
-    
-    
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long id) {
+        return employeeService.getEmployeeById(id).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employee) {
+        return employeeService.getEmployeeById(id)
+                .map(savedEmployee -> {
+                    savedEmployee.setFirstName(employee.getFirstName());
+                    savedEmployee.setLastName(employee.getLastName());
+                    savedEmployee.setEmail(employee.getEmail());
+                    Employee updatedEmployee = employeeService.update(savedEmployee);
+                    return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<String>("Employee delete Sucessfully!.", HttpStatus.OK);
+
+    }
+
 }
